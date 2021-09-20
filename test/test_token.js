39cf4dd674bef61c9001507b63d55f81c8f6e04e
Alexandre Melo <amelo@harlemnext.com>
2021-09-20 10:58:18 -0300

    Test file

--- test/test_token.js
+++ test/test_token.js
@@ -9,21 +9,28 @@ describe('ETH MINT BURN', () => {
         Token = await ethers.getContractFactory('NoFeeToken');
         [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
         token = await Token.deploy(
-            'test', 'TESTX', 1_000_000
+            'test', 'TESTX', ethers.utils.parseUnits("1000000", "gwei")
         );
 
         Vault = await ethers.getContractFactory('Vault');
         vault = await Vault.deploy();
+
+        const bag = ethers.utils.parseUnits("250000", "gwei")
+
+        token.transfer(addr1.address, bag);
+        token.transfer(addr2.address, bag);
     });
 
 
     describe('Use vault', () => {
 
         it('Happy path', async () => {
-            console.log(token);
-            //console.log(vault);
+            console.log((await token.totalSupply()).toString());
+            console.log(parseInt(await token.balanceOf(addr1.address)));
+            console.log(parseInt(await token.balanceOf(addr2.address)));
+            console.log(parseInt(await token.balanceOf(owner.address)));
+            console.log(parseInt(await token.balanceOf(vault.address)));
         });
-
     });
 
 });
