3a3c2ea4f3fe3532de2cbe16ef999ea278274969
Alexandre Melo <amelo@harlemnext.com>
2021-11-09 18:55:23 -0300

    new test

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "hardhat/console.sol";
 
 contract Vault is Ownable {
     using SafeERC20 for IERC20;
@@ -83,6 +84,9 @@ contract Vault is Ownable {
         uint256 _minLockDays,
         uint256 _amount
     ) external returns (uint256) {
+
+        console.log('print');
+
         require(vaultKeys[key] == 0, "Vault Key Already used");
         require(_tokenReward.balanceOf(msg.sender) >= _amount, "User has no tokens");
         require(_vaultDays > 0, "Vault days zero");
